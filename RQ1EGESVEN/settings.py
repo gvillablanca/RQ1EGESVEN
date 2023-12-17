90b6a31c894a72d7c7ec7d2876900bc89128bcdc
gvillablanca <g.villalobo.m@gmail.com>
2023-12-17 18:18:20 -0300

    conexion base de datos

--- RQ1EGESVEN/settings.py
+++ RQ1EGESVEN/settings.py
@@ -77,14 +77,20 @@ WSGI_APPLICATION = 'RQ1EGESVEN.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#database local
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/xe',
+        'USER': 'eGESVEN_ADMIN',
+        'PASSWORD': 'Q1w2e3r4t5y6',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
 }
-
-#
 
 
 # Password validation
